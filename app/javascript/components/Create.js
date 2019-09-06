1d5e5a8ee88ade32b6fe42703dbef8d063e3e995
julie <jfryer8726@gmail.com>
2019-09-06 11:26:54 -0400

    basic stuff

---
+++ app/javascript/components/Create.js
@@ -0,0 +1,14 @@
+//packages
+import React from "react"
+
+//component class
+class Create extends React.Component {
+render () {
+  return (
+    <h1>create form</h1>
+  )
+}
+}
+
+//export
+export default Create
