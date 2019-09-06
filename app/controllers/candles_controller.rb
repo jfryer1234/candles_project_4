078dbb615572c5594e170d37547e5b96c84b0a08
Becka Catanzaro <becka@Beckas-MacBook-Pro.local>
2019-09-06 13:20:55 -0400

    added show and create routes

--- app/controllers/candles_controller.rb
+++ app/controllers/candles_controller.rb
@@ -6,14 +6,18 @@ class CandlesController < ApplicationController
   end
 
   def show
+    render json: Candle.find(params["id"])
   end
 
   def create
+    render json: Candle.create(params["candle"])
   end
 
   def delete
+    render json: Candle.delete(params["id"])
   end
 
   def update
+    render json: Candle.update(params["id"], params["candle"])
   end
 end
