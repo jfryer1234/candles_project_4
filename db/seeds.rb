0fa768a5bc149a1be18337be92ba2cfc9616d63c
Becka Catanzaro <becka@Beckas-MacBook-Pro.local>
2019-09-08 12:35:06 -0400

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Candle.create({
+  "scent_1" => "cinnamon",
+  "scent_2" => "no_scent",
+  "embellishment" => "none",
+  "color" => "orange",
+  "packaging" => "taper"
+  })
+Candle.create({
+  "scent_1" => "fresh_linen",
+  "scent_2" => "lavender",
+  "embellishment" => "extra_bubbles",
+  "color" => "yellow",
+  "packaging" => "glass_jar"
+  })
+Candle.create({
+  "scent_1" => "citrus",
+  "scent_2" => "ginger",
+  "embellishment" => "rose_petals",
+  "color" => "red",
+  "packaging" => "three_wick"
+  })
